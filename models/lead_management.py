a7c0c51759c0b45f5981a8999ec1abf85cbf0556
christapherct <christapherct02@gmail.com>
2022-09-30 15:11:36 +0530

    Lost reason of lead added into the lead page

--- models/lead_management.py
+++ models/lead_management.py
@@ -12,7 +12,7 @@ class LeadManagement(models.Model):
     lost_reason_ids = fields.One2many('lead.lost.reason', 'lead_management_id')
     name = fields.Char(string="Lead Name", required=True, track_visibility=True)
     customer_id = fields.Many2one('client.management', string="Customer Name", required=True, track_visibility=True)
-    # customer_address = fields.Char(related="customer_id.address", string="Customer Address", track_visibility=True, readonly=False)
+    customer_address = fields.Text(related="customer_id.address", string="Customer Address", track_visibility=True, readonly=False)
     customer_phone = fields.Char(related="customer_id.contact", string="Customer Phone", track_visibility=True, readonly=False)
     customer_mail = fields.Char(related="customer_id.contact", string="Customer Email", track_visibility=True, readonly=False)
     customer_website = fields.Char(related="customer_id.website", string="Website", track_visibility=True, readonly=False)
@@ -25,22 +25,32 @@ class LeadManagement(models.Model):
     ended_date = fields.Datetime(string="End Date", readonly=True)
     stage = fields.Selection(
         selection=[('new', 'New'), ('postponed', 'Postponed'), ('project', 'Project'), ('lost', 'Lost')], track_visibility=True)
-    lost_reason = fields.Text(string="Lost Reason")
+    lost_reason = fields.Text(string="Lost Reason", readonly=True)
 
-    def action_lost(self):
-        return
+    def action_lost_lead(self):
+            return {
+                'res_model': 'lead.lost.reason',
+                'type': 'ir.actions.act_window',
+                'context': {'default_lead_management_id': self.id},
+                'view_mode': 'form',
+                'view_type': 'form',
+                'target': 'new',
+            }
 
 
 class LostLeadManagement(models.TransientModel):
     _name = 'lead.lost.reason'
     _inherit = ['mail.thread']
 
-    reason = fields.Text(string="Reason", track_visibility=True)
+    reason = fields.Text(string="Reason", track_visibility=True, required=True)
     lead_management_id = fields.Many2one('lead.management')
 
-    def rejection_reason(self):
+    def lead_lost_reason(self):
+        print("Haii")
+        print(self.lead_management_id.id)
+        print(self.id)
         for rec in self.lead_management_id:
             print("jjjjj")
             rec.stage = 'lost'
             rec.lost_reason = self.reason
-            rec.ended_date = fields.Date.today()
+            rec.ended_date = datetime.today()
